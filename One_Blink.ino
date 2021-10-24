a901eea70d741389d7595e190b12f55793b71c30
Benjamin Nguyen <90567404+ben-30@users.noreply.github.com>
2021-10-24 02:55:35 -0400

    add source code

---
+++ One_Blink.ino
@@ -0,0 +1,18 @@
+void setup() {
+  pinMode(9, INPUT);      // sets the digital pin 9 as input
+  pinMode(8, OUTPUT);     // sets the digital pin 8 as output
+}
+
+void loop() {
+  int val = digitalRead(9);
+  if (val = HIGH) {
+    digitalWrite(8, HIGH);  // sets the digital pin 8 on
+    delay(1000);            // waits for a second
+    digitalWrite(8, LOW);   // sets the digital pin 8 off
+    delay(1000);            // waits for a second
+  }
+  else {
+    digitalWrite(8, LOW);
+    delay(1000);
+  }
+}
---
+++ Three_Blink.ino
@@ -0,0 +1,29 @@
+void setup() {
+  pinMode(9, INPUT);       // sets the digital pin 9 as input
+  pinMode(10, OUTPUT);     // sets the digital pin 10 as output
+  pinMode(11, OUTPUT);     // sets the digital pin 11 as output
+  pinMode(12, OUTPUT);     // sets the digital pin 12 as output
+}
+
+void loop() {
+  if (digitalRead(9) = HIGH) {
+    digitalWrite(10, HIGH);  // sets the digital pin 10 on
+    delay(1000);             // waits for a second
+    digitalWrite(10, LOW);   // sets the digital pin 10 off
+    delay(1000);             // waits for a second
+    digitalWrite(11, HIGH);  // sets the digital pin 11 on
+    delay(1000);             // waits for a second
+    digitalWrite(11, LOW);   // sets the digital pin 11 off
+    delay(1000);             // waits for a second
+    digitalWrite(12, HIGH);  // sets the digital pin 12 on
+    delay(1000);             // waits for a second
+    digitalWrite(12, LOW);   // sets the digital pin 12 off
+    delay(1000);             // waits for a second
+  }
+  else {
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    delay(1000);
+  }
+}
